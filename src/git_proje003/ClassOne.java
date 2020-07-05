290f14b6083770bedfa5dee4f246b39b6091a3a5
Iskender <67157017+aselyacan@users.noreply.github.com>
2020-07-05 20:55:30 +0200

    Update ClassOne.java
    remote dan ilk commit denemesi

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("Local master branch");
 		System.out.println("local master branch ikinci commit");
+		System.out.println("remote dan ilk commit ")
 
 	}
 
