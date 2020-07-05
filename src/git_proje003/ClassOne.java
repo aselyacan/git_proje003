e8c4640bd4f399a0e19cce7f5646560a8aa36985
Iskender <67157017+aselyacan@users.noreply.github.com>
2020-07-05 20:57:27 +0200

    Update ClassOne.java

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -5,7 +5,8 @@ public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("Local master branch");
 		System.out.println("local master branch ikinci commit");
-		System.out.println("remote dan ilk commit ")
+		System.out.println("remote dan ilk commit ");
+		System.out.println("'remote dan ikinci commit");
 
 	}
 
