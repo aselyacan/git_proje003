02fc0fbddbd75f9c468ee66c47c6a1c51cf2f120
aselyacan <iskender1982@gmail.com>
2020-07-05 20:59:17 +0200

    localdan ucuncu commit

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -7,6 +7,7 @@ public class ClassOne {
 		System.out.println("local master branch ikinci commit");
 		System.out.println("remote dan ilk commit ");
 		System.out.println("'remote dan ikinci commit");
+		System.out.println("Local masterdan ucuncu commit");
 
 	}
 
