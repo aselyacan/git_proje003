2d4fd73f3da2bd80d5cc0d35044c8309dbaf31b9
aselyacan <iskender1982@gmail.com>
2020-07-06 12:17:06 +0200

    ilk commit feature branchten

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -5,7 +5,14 @@ public class ClassOne {
 	public static void main(String[] args) {
 		
 		System.out.println("local remote den ilk commit");
-		System.out.println("local dan  master dan branch 2. commit ");
+		System.out.println("local dan  feature  dan branch 2. commit ");
+		
+		System.out.println("local feauture branch dan ilk commit ");
+		int as=34;
+		int df=23;
+		
+		System.out.println(as+df+"feature  icin ilk conflickt denemesi");
+		
 		
 	}
 
