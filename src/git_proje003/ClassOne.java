effa6c69439843751501cc0bd19544f161025e74
aselyacan <iskender1982@gmail.com>
2020-07-06 11:33:42 +0200

    ikinci commit local masterdan

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -3,12 +3,10 @@ package git_proje003;
 public class ClassOne {
 
 	public static void main(String[] args) {
-		System.out.println("Local master branch");
-		System.out.println("local master branch ikinci commit");
-		System.out.println("remote dan ilk commit ");
-		System.out.println("'remote dan ikinci commit");
-		System.out.println("Local masterdan ucuncu commit");
-
+		
+		System.out.println("local remote den ilk commit");
+		System.out.println("local dan  master dan branch 2. commit ");
+		
 	}
 
 }
